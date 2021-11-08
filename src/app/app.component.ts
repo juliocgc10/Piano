3635e87c563e8c5b867abf814e63f40d2cddfe6a
Julio César Guzmán Cortés <juliocgc10@hotmail.com>
2021-11-07 23:40:22 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Piano';
+
+  applySound(number: number): void{
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note'+number+'.wav';
+    audio.load();
+    audio.play();
+  }
+
+}
